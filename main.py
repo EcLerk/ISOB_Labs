def3dd8a833a8bfab36cf56f715c022becbb7012
Lera <lerakachanovskaya@gmail.com>
2024-01-26 23:22:03 +0300

    Fix encryption

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
-ALPHABET = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-N = 66
+ALPHABET = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+N = 33
+
 
 def encrypt():
-    with open('data.txt','r', encoding='utf-8') as f:
+    with open('data.txt', 'r', encoding='utf-8') as f:
         data = f.readlines()
 
     key = int(data[0])
-    str = data[1]
+    string = data[1]
     encrypted_str = ''
 
-    for i in str:
-        curr_index = ALPHABET.find(i)
+    for i in string:
+        is_upper = i.isupper()
+        curr_index = ALPHABET.find(i.lower())
+
         if curr_index == -1:
-           encrypted_str += i
+            encrypted_str += i.upper() if is_upper else i
         else:
-            encrypted_str += ALPHABET[(curr_index + key) % N]
+            encrypted_str += ALPHABET[(curr_index + key) % N].upper() if is_upper else ALPHABET[(curr_index + key) % N]
 
     return encrypted_str
 
-#def decrypt():
+
+def decrypt():
+    
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
