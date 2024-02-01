530986b959acc1bbb381a9f24344114b8a786f0c
Lera <lerakachanovskaya@gmail.com>
2024-02-01 15:41:49 +0300

    Add Vigenere encryption

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 ALPHABET = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 N = 33
+ALPHABET_EN = 'abcdefghijklmnopqrstuvwxyz'
+N_EN = 26
 
 
 def get_data(filename):
     with open(filename, 'r', encoding='utf-8') as f:
         data = f.readlines()
 
-    key = int(data[0])
+    key = data[0]
     string = data[1]
     return key, string
 
 
-def encrypt(key, string):
+def encrypt_cesar(key, string):
     encrypted_str = ''
 
     for i in string:
@@ -26,7 +28,7 @@ def encrypt(key, string):
     return encrypted_str
 
 
-def decrypt(key, string):
+def decrypt_cesar(key, string):
     decrypted_str = ''
 
     for i in string:
@@ -41,6 +43,32 @@ def decrypt(key, string):
 
     return decrypted_str
 
+
+def generate_key(key, string):
+    key = list(key)
+    if len(string) == len(key):
+        return key
+    else:
+        for i in range(len(string) - len(key)):
+            key.append(key[i % len(key)])
+
+    return "" . join(key)
+
+
+def encrypt_vigenere(key, string):
+    key = generate_key(key, string)
+    encrypted_str = ''
+
+    for i in range(len(string)):
+        index = ALPHABET_EN.find(string[i])
+        if index == -1:
+            encrypted_str += string[i]
+        else:
+            encrypted_str += ALPHABET_EN[(index + ALPHABET_EN.find(key[i])) % N_EN]
+
+    return encrypted_str
+
+
 def main():
     while True:
         print('Выберете действие:\n'
@@ -54,14 +82,18 @@ def main():
 
         if action == '1':
             key, string = get_data(encryption_path)
-            print(encrypt(key, string) + '\n')
+            print(encrypt_cesar(int(key), string) + '\n')
         elif action == '2':
             key, string = get_data(decryption_path)
-            print(decrypt(key, string) + '\n')
+            print(decrypt_cesar(int(key), string) + '\n')
+        elif action == '3':
+            key, string = get_data(encryption_path)
+            print(encrypt_vigenere(key, string) + '\n')
         elif action == '0':
             return
 
-    # Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
     encryption_path = "encryption.txt"
     decryption_path = "decryption.txt"
